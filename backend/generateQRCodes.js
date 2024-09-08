c2747b3ab04dee0ee512cd0892df871bf0d6cc6b
Mishismail <mishkaismail@icloud.com>
2024-09-08 16:23:43 +0200

    "update qr codes"

--- backend/generateQRCodes.js
+++ backend/generateQRCodes.js
@@ -1,12 +1,13 @@
 const generateQRCode = require('./utils/qrCodeGenerator');
 
 // Base URL of the check-in page running on localhost:3001 or deployment URL
-const BASE_URL = 'http://localhost:3001/check-in';
+const BASE_URL = 'https://odwh-check-in.netlify.app/check-in';
 
 const locations = [
   'Oude-Westhof-H001',
   'Oude-Westhof-H002',
   'Oude-Westhof-H003',
+  'Oude-Westhof-H004',
 ];
 
 // Generate QR codes for each location
