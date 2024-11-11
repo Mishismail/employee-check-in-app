4ceecd657db89fa5482231d5b683aa00196def61
Mishismail <mishkaismail@icloud.com>
2024-11-11 14:15:06 +0200

    rename backend server to api

--- checkin-page/src/api.js
+++ checkin-page/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',  // Backend running on port 8080
+  baseURL: 'https://employee-check-in-app-backend.vercel.app/api', 
 });
 
 export default api;
